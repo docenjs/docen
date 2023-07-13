fcb88867b33f1b1018355dde3b9e85794a314430
Demo Macro <abc@imst.xyz>
2023-07-14 00:10:00 +0800

    feat: use office-text-extractor to extract text

--- packages/docen/bundle.mjs
+++ packages/docen/bundle.mjs
@@ -6,16 +6,16 @@ execSync("pnpm prepack", { stdio: "inherit" });
 execSync("pnpm ncc build dist/cli.cjs -o dist/ncc", { stdio: "inherit" });
 
 const version = readFileSync("package.json", "utf-8").match(
-  /"version": "(.*?)"/
+  /"version": "(.*?)"/,
 )[1];
 
-execSync(`cp dist/ncc/index.cjs dist/ncc/docen-${version}.cjs`, {
+execSync(`mv dist/ncc/index.cjs dist/ncc/docen-${version}.cjs`, {
   stdio: "inherit",
 });
 
 execSync(
-  `pnpm dlx pkg dist/ncc/docen-${version}.cjs --out-path dist/bundle -C GZip`,
+  `pnpm dlx pkg dist/ncc/docen-${version}.cjs --out-path dist/bundle -c pkg.config.json -C GZip`,
   {
     stdio: "inherit",
-  }
+  },
 );
