c54aef1223905b955ba98bf814f472383c75891c
Demo Macro <abc@imst.xyz>
2023-08-29 10:21:55 +0800

    build: avoid executable files being packaged into npm published packages

--- packages/docen/bundle.mjs
+++ packages/docen/bundle.mjs
@@ -3,23 +3,23 @@ import { readFileSync } from "fs";
 
 execSync("pnpm prepack", { stdio: "inherit" });
 
-execSync("pnpm ncc build dist/cli.cjs -o dist/ncc -a", { stdio: "inherit" });
+execSync("pnpm ncc build dist/cli.cjs -o .cache/ncc -a", { stdio: "inherit" });
 
 const version = readFileSync("package.json", "utf-8").match(
   /"version": "(.*?)"/,
 )[1];
 
-execSync(`mv dist/ncc/index.cjs dist/ncc/docen-${version}.js`, {
+execSync(`mv .cache/ncc/index.cjs .cache/ncc/docen-${version}.js`, {
   stdio: "inherit",
 });
 
 execSync(
-  "cp node_modules/pdfjs-dist/build/pdf.worker.js dist/ncc/pdf.worker.js",
+  "cp node_modules/pdfjs-dist/build/pdf.worker.js .cache/ncc/pdf.worker.js",
   { stdio: "inherit" },
 );
 
 execSync(
-  `pnpm dlx pkg dist/ncc/docen-${version}.js --out-path dist/bundle -c pkg.config.json -C GZip`,
+  `pnpm dlx pkg .cache/ncc/docen-${version}.js --out-path .cache/bundle -c pkg.config.json -C GZip`,
   {
     stdio: "inherit",
   },
