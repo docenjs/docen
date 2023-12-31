930adfff1da435714609f14a711001c064ac71e9
Demo Macro <abc@imst.xyz>
2023-12-31 16:44:01 +0800

    feat: support extractImageFromPDF

--- packages/app/src-tauri/src/main.rs
+++ packages/app/src-tauri/src/main.rs
@@ -1,20 +1,10 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
 
 fn main() {
   tauri::Builder::default()
-    .setup(|app| {
-      match app.get_cli_matches() {
-        // `matches` here is a Struct with { args, subcommand }.
-        // `args` is `HashMap<String, ArgData>` where `ArgData` is a struct with { value, occurrences }.
-        // `subcommand` is `Option<Box<SubcommandMatches>>` where `SubcommandMatches` is a struct with { name, matches }.
-        Ok(matches) => {
-          println!("{:?}", matches)
-        }
-        Err(_) => {}
-      }
-      Ok(())
-    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
