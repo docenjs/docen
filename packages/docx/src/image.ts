0ebffcf1f7a5d2f304eca588ea3e8649b1315205
Demo Macro <abc@imst.xyz>
2024-01-12 22:25:03 +0800

    perf: reduce JSZIP reads to provide performance

--- packages/docx/src/image.ts
+++ packages/docx/src/image.ts
@@ -1,22 +1,18 @@
 import JSZIP from "jszip";
 
 export async function extractImageFromDocx(docxSource: Uint8Array) {
-  const files = Object.keys((await JSZIP.loadAsync(docxSource)).files);
+  const files = (await JSZIP.loadAsync(docxSource))?.files;
 
   const images: {
     name: string;
     buffer: Uint8Array;
   }[] = [];
 
-  for (const name of files) {
-    if (name.startsWith("word/media/")) {
-      const buffer = (await (await JSZIP.loadAsync(docxSource))
-        .file(name)
-        ?.async("uint8array")) as Uint8Array;
-
+  for (const fileName in files) {
+    if (fileName.startsWith("word/media/")) {
       images.push({
-        name: name.replace("word/media/", ""),
-        buffer,
+        name: fileName.replace("word/media/", ""),
+        buffer: await files[fileName]?.async("uint8array"),
       });
     }
   }
