0906525990625efbb214e2a69a94ef77ba624dc7
Demo Macro <abc@imst.xyz>
2024-01-13 23:09:00 +0800

    feat: support replace styles for docx

--- packages/docx/src/document.ts
+++ packages/docx/src/document.ts
@@ -5,3 +5,24 @@ export async function getDocumentXML(docxSource: Uint8Array) {
     zip?.file("word/document.xml")?.async("text")
   );
 }
+
+export async function getStylesXML(docxSource: Uint8Array) {
+  return JSZIP.loadAsync(docxSource).then((zip) =>
+    zip?.file("word/styles.xml")?.async("text")
+  );
+}
+
+export async function replaceStylesXML(
+  docxSource: Uint8Array,
+  docxTarget: Uint8Array
+) {
+  const sourceStyles = await getStylesXML(docxSource);
+
+  const targetZip = await JSZIP.loadAsync(docxTarget);
+
+  if (sourceStyles) {
+    targetZip.file("word/styles.xml", sourceStyles);
+  }
+
+  return targetZip.generateAsync({ type: "uint8array" });
+}
